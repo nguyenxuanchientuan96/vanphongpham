76add7748a091d139940da3f1bdf242522709fed
Nguyen Xuan Chien Tuan <you@example.com>
2017-05-10 12:09:54 +0700

    update 10/05

--- template-parts/home.php
+++ template-parts/home.php
@@ -1,10 +1,28 @@
 		<div class="body-left">
-			<?php 
+		<!-- 	<?php 
 				$sidebar = get_config("left-sidebar");
 				if( $sidebar ){
 					echo $sidebar;
 				}
-			?>	
+			?> -->	
+			<div class="body-left-menu">
+				<ul>
+					<li><a href="index.php?r=home" title="home">TRANG CHỦ</a></li>
+					<li><a href="#" title="gioithieu">GIỚI THIỆU</a></li>
+
+				<?php 
+					$sql = "SELECT * FROM menu";
+					$rs = mysqli_query($conn, $sql);
+					while( $r = mysqli_fetch_array($rs, MYSQLI_ASSOC) ):
+				?>
+					<li><a href="index.php?r=category&id=<?php echo $r['id']; ?>" title="<?php echo $r['info']?>"><?php echo $r["name"];?></a></li>
+
+				<?php endwhile;?>
+
+				<li><a href="index.php?r=about" title="lienhe">LIÊN HỆ</a></li>	
+				<li><a href="admin">ĐĂNG NHẬP QUẢN TRỊ</a></li>
+			</ul>
+			</div>
 		</div><!---end .body-right-->
 
 
@@ -29,9 +47,23 @@
 						
 						
 						<div class="block-product">	
-							<div class="block-body-item">
-								<img src="upload/<?php echo $r["avatar"] ?>" title="<?php echo $r["name"];?>" alt="<?php echo $r["name"]; ?>" style="width=100% ; max-width:200px ;  height:50%; ; display:block; padding-top: 5px; "/>
-									
+							<div class="block-body-item" style="border:0.5px solid grey">
+								<div class="block-body-item-img" style="width: 100%; height:65%;overflow: hidden; ">
+									<img src="upload/<?php echo $r["avatar"] ?>" title="<?php echo $r["name"];?>" alt="<?php echo $r["name"]; ?>" style="width: 100%; max-width:230px ;  height:90%; ; display:block; padding-top: 15px; 
+										 transition-duration: 0.5s;  /*sẽ chứa gía trị là thời gian(đơn vị s = giây) nó sẽ thiết lập quá trình để phóng to thumbnail mất bao nhiêu thời gian*/
+								        /* Safari & Google Chrome */
+								        -webkit-transition-duration: 0.5s; 
+								        /* Mozilla Firefox */
+								        -moz-transition-duration: 0.5s; 
+								        /* Opera */
+								        -o-transition-duration: 0.5s;
+								        /* IE 9 */
+								        -ms-transition-duration: 0.5s;
+
+
+
+									 "/>
+								</div>
 									<p class="block-body-title">
 										<a href="index.php?r=single&id=<?php echo $r['id'];?>"><?php echo $r["name"];?></a>
 									</p>
@@ -42,7 +74,7 @@
 									<p class="price">
 										</br><a href="index.php?r=cart&action=add&id=<?php echo $r['id']; ?>"><?php echo $r["price"] ."(đ)";?></a>
 									</p>
-									<p class="block-body-button-tieu-bieu">
+									<p class="block-body-button-tieu-bieu" style="margin-bottom: 5px;">
 										<a href="index.php?r=cart&action=add&id=<?php echo $r['id']; ?>">MUA</a>
 									</p>
 							</div>
